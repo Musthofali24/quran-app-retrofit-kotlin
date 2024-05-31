4064b5d4e2fa8476dc7f049da30b104b32eaf15d
Musthofali24 <almus2610@gmail.com>
2024-05-31 15:36:24 +0700

    first commit

---
+++ app/src/main/java/com/example/aplikasialquran/model/ModelSurah.kt
@@ -0,0 +1,13 @@
+package com.example.aplikasialquran.model
+
+import java.io.Serializable
+
+class ModelSurah : Serializable {
+    var arti: String? = null
+    var asma: String? = null
+    var ayat: String? = null
+    var nama: String? = null
+    var type: String? = null
+    var nomor: String? = null
+    var keterangan: String? = null
+}
